9effe56631bebfe1fb239f8068f30f74ff883bc4
Irinaabieva <irinaabieva03@gmail.com>
2022-11-16 15:22:23 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        // Задание 1
+        int[] arr1 = {1 , 2 , 3};
+        double[] arr2 = {1.57, 7,654, 9,986};
+        char[] arr3 = {'a', 'b', 'c'};
     }
 }
